8157c4744d8daaef8c6394abb7e71bf47f5edd46
adachi <dutch@adachis.info>
2019-12-24 17:01:17 -0500

    added readme.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,34 +1,25 @@
 import React from 'react';
-import '../styles/index.scss'
-function getValue(){
-    return Promise.resolve('this is value');
-}
-const App=()=>{
-    const [value,setValue]=React.useState('');
-    getValue().then(d=>{console.log(d); setValue(d)}); //check if Promise work
-    console.log("this is a pen".includes('pen'));  //check if array.includes work
-    const three=["1","2","3","4"].find((a)=>(a==="3"));  // check if array.find work
-    const searchParamString = location.search;   // check if URLSearchPrams work
-    const searchParam=new URLSearchParams(searchParamString);
-    for (const [key,value] of searchParam){
-        console.log('key:',key,' value:',value)
+import '../styles/index.scss';
+
+const App=(prop:{value:string})=>{
+
+        const three=["1","2","3","4"].find((a)=>(a==="3"));  // check if array.find work
+        return  (
+
+            <div>
+                <h1>Hello world!!</h1>
+                <p>{prop.value}</p>
+                <p>String based on array.prototype.find: {three?'found':'not found'}</p>
+                <p>Promise supported!</p>
+            </div>
+
+        )
+
     }
-   return  (
-
-        <div>
-            <h1>Hello world</h1>
-            <p>{value}</p>
-            <p>String based on array.prototype.find: {three?'found':'not found'}</p>
-
-        </div>
-
-    )
-
-}
 
 
 
 
-     ;
+;
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,21 @@ import ReactDom from 'react-dom'
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 // above two imports to provide compatibility with older browsers (IE 11)
-import App from './components/App'
 
-ReactDom.render(<App />, document.getElementById('root'));
+import App from './components/App'
+const searchParamString = location.search;   // check if URLSearchPrams work
+const searchParam=new URLSearchParams(searchParamString);
+
+console.log('array.prototype.includes supported ',"this is a pen".includes('pen'));  //check if array.includes work
+console.log('urlparams supported:');
+for (const [key,value] of searchParam){
+    console.log('key:',key,' value:',value)
+}
+function getValue(){
+    return Promise.resolve('this is value');
+}
+
+getValue().then((p)=>
+    {return ReactDom.render(<App value={p}/>, document.getElementById('root'))}
+
+);
